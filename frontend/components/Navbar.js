28031df861446cf6cce948dc36d29f732af6a424
Gustavo Nunes <gustavompn06@gmail.com>
2026-07-14 06:33:28 -0300

    feat: navbar component

---
+++ frontend/components/Navbar.js
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Início</Link>
+        </li>
+
+        <li>
+          <Link href="/entrada">Entrada</Link>
+        </li>
+
+        <li>
+          <a href="#" onClick={(e) => e.preventDefault()}>
+            Saída
+          </a>
+
+          <ul className={styles.dropdown}>
+            <li>
+              <Link href="/venda">Venda</Link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="#" onClick={(e) => e.preventDefault()}>
+            Cadastro
+          </a>
+
+          <ul className={styles.dropdown}>
+            <li>
+              <Link href="/cadastro/despesas">Despesas</Link>
+            </li>
+
+            <li>
+              <Link href="/cadastro/cliente">Cliente</Link>
+            </li>
+
+            <li>
+              <Link href="/cadastro/produto">Produto</Link>
+            </li>
+
+            <li>
+              <Link href="/cadastro/fornecedor">Fornecedor</Link>
+            </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href="#" onClick={(e) => e.preventDefault()}>
+            Registros
+          </a>
+
+          <ul className={styles.dropdown}>
+            <li>
+              <Link href="/registro/vendas">Vendas</Link>
+            </li>
+
+            <li>
+              <Link href="/registro/encomendas">Encomendas</Link>
+            </li>
+
+            <li>
+              <Link href="/estoque">Estoque</Link>
+            </li>
+
+            <li>
+              <Link href="/relatorios">Relatórios</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
